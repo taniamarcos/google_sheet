85781f023effbb0399bcf242bb1546d4dbe60160
tania <alumne@tania-pc.insebre.cat>
2022-05-31 15:18:43 +0200

    projecte google sheet tania

--- config/sheets.php
+++ config/sheets.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'post_spreadsheet_id' => "1N7dQIB1E3acOAzCkL9mEwuJ1byKG8Dr2Ulzmepp6H0Q",
+    'post_spreadsheet_id' => "1K0frp8GebGgO-cNRU3FL7H2c_XtovCLD-VRNSCmJjnk",
 
-    'post_sheet_id' => "Register"
+    'post_sheet_id' => "sheet"
 ];
